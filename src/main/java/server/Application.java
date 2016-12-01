544dcb4d6bbd0819040fc9450a542fdb2ef10e29
ByMiles <lorenzmiles@gmail.com>
2016-12-01 04:17:59 +0100

    changed name + added close-frame

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -3,10 +3,18 @@ package server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Rest-Service-ReversiByMiles ;-)");
+        frame.setSize(300, 100);
+        frame.add(new JLabel("close me to quit server-service!"));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         SpringApplication.run(Application.class, args);
     }
 }
